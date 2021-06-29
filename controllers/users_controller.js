071e86ea8aa6ae8a628e5623cf107bf496394488
Pratham Bansal <prathambansal29@gmail.com>
2021-06-29 13:25:55 +0530

    database added and simple sign in also added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,27 @@ module.exports.profile = function(req,res){
         title : "Profile"
     });
 }
+
+//render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up', {
+        title : "Codeial | Sign Up"
+    });
+}
+
+//render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in', {
+        title : "Codeial | Sign In"
+    });
+}
+
+//get the sign up data
+module.exports.create = function(req,res){
+    //TODO Later
+}
+
+//get the sign in data
+module.exports.createSession = function(req,res){
+    //TODO Later
+}
--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,7 @@ const postController = require('../controllers/post_controller');
 router.get('/profile', usersController.profile);
 router.get('/post',postController.post);
 
+router.get('/sign-up' , usersController.signUp);
+router.get('/sign-in',usersController.signIn);
+
 module.exports = router;
