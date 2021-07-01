fe966630e8016de5fdbc1db72d9c828da8ccd7b9
Pratham Bansal <prathambansal29@gmail.com>
2021-07-02 00:14:25 +0530

    passport js all done until here

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,6 +10,9 @@ module.exports.profile = function(req,res){
 
 //render the sign up page
 module.exports.signUp = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_up', {
         title : "Codeial | Sign Up"
     });
@@ -17,6 +20,9 @@ module.exports.signUp = function(req,res){
 
 //render the sign in page
 module.exports.signIn = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in', {
         title : "Codeial | Sign In"
     });
@@ -51,4 +57,10 @@ module.exports.create = function(req,res){
 //get the sign in data
 module.exports.createSession = function(req,res){
     //TODO Later
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req,res){
+    req.logout();
+    return res.redirect('/');
 }
