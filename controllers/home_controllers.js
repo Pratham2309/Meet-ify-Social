d0095f0eb80678cb88cd89ba6339235defdfafcf
Pratham Bansal <prathambansal29@gmail.com>
2021-07-02 14:22:36 +0530

    done until the scss and sass

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,9 +1,9 @@
 module.exports.home = function(req,res){
     console.log(req.cookies);
-    res.cookie('user_id',25);
+    // res.cookie('user_id',25);
     // return res.end("<h1>Express is up for Codeial</h1>");
     return res.render('home',{
-        title : "Home"
+        title : "Home Page"
     }); 
 }
  
