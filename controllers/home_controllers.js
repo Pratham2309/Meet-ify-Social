03bbf56e4284b24496f6a53843cc6fce2791174b
Pratham Bansal <prathambansal29@gmail.com>
2021-07-04 12:51:40 +0530

    added friends list in the home page and styled it better

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,6 +1,7 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
-module.exports.home = function(req,res){
+module.exports.home = function (req, res) {
     // console.log(req.cookies);
     // res.cookie('user_id',25);
     // return res.end("<h1>Express is up for Codeial</h1>");
@@ -14,19 +15,22 @@ module.exports.home = function(req,res){
 
     //populate the user of each post
     Post.find({})
-    .populate('user')
-    .populate({
-        path : 'comments',
-        populate : {
-            path : 'user'
-        }
-    })
-    .exec(function(err,posts){
-        return res.render('home',{
-            title : "Home Page",
-            posts : posts
-        }); 
-    });
-    
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+        .exec(function (err, posts) {
+            User.find({}, function (err, users) {
+                    return res.render('home', {
+                    title: "Home Page",
+                    posts: posts,
+                    all_users : users
+                });
+            });
+
+        });
+
 }
- 
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,9 +3,31 @@ const User = require('../models/user')
 
 module.exports.profile = function(req,res){
     // res.end("<h1>User Profile</h1>");
-    return res.render('user_profile',{
-        title : "Profile"
+    User.findById(req.params.id, function(err,user){
+        if(user){
+            return res.render('user_profile',{
+                title : "User Profile",
+                profile_user : user
+            });
+        }
+        else{
+            return res.render('user_profile',{
+                title : "User Profile",
+                profile_user : req.user
+            });
+        }
     });
+}
+
+module.exports.update = function(req,res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body, function(err,user){
+            return res.redirect('back');
+        });
+    }
+    else{
+        return res.status(401).send('Unauthorized');
+    }
 }
 
 //render the sign up page
