d9799c0b2219cda325368010a358614f28d9bd6f
Pratham Bansal <prathambansal29@gmail.com>
2021-07-07 10:23:13 +0530

    added sign in and sign up using the google portal

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,46 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+
+
+//tell passport to use a new strategy for ggoogle login
+passport.use(new googleStrategy({
+        clientID : "878697946388-kr91bd0okjoaaubg0gm15v2v8m3h9crl.apps.googleusercontent.com",
+        clientSecret : "EHaxqVoW32TCzRWaDjxj0ooS",
+        callbackURL : "http://localhost:8000/users/auth/google/callback"
+    } , 
+
+    function(accessToken , refreshToken, profile , done){
+        User.findOne({email : profile.emails[0].value}).exec(function(err,user){
+            if(err){
+                console.log('Error in google strategy-passport' , err);
+                return;
+            }
+
+            console.log(profile);
+
+            if(user){
+                //if found set this user as req.user
+                return done(null,user);
+            }
+            else{
+                //if not found, create the user and set it as req.user
+                User.create({
+                    name : profile.displayName,
+                    email : profile.emails[0].value,
+                    password : crypto.randomBytes(20).toString('hex')
+                }, function(err,user){
+                    if(err){
+                        console.log('Error in creating user in google strategy-passport' , err);
+                        return;
+                    }
+                    return done(null,user);
+                })
+            }
+        });
+    }
+
+));
+
+module.exports = passport;
