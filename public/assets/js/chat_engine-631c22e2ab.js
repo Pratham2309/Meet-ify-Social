ee070312687c7347fb55a6da2113d4f71479df8a
Pratham Bansal <prathambansal29@gmail.com>
2021-07-12 14:30:31 +0530

    added gulp to minify the css and javascript files

---
+++ public/assets/js/chat_engine-631c22e2ab.js
@@ -0,0 +1,1 @@
+class ChatEngine{constructor(n,t){this.chatBox=$("#"+n),this.userEmail=t,this.socket=io.connect("http://localhost:5000"),this.userEmail&&this.connectionHandler()}connectionHandler(){this.socket.on("connect",(function(){console.log("connection established using sockets...!")}))}}
